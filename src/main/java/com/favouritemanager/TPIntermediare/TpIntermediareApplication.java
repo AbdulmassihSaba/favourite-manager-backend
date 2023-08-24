65825a9a6655caf75baccdb6aa79af0f1d1c803c
Abdulmassih <abdalmassehsaba@gmail.com>
2023-08-24 10:44:06 +0200

    Initial Commit

---
+++ src/main/java/com/favouritemanager/TPIntermediare/TpIntermediareApplication.java
@@ -0,0 +1,13 @@
+package com.favouritemanager.TPIntermediare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntermediareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntermediareApplication.class, args);
+	}
+
+}
