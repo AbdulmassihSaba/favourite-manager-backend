60e9e02c738cbe4f758b20cfc2a5228e27fd923b
Abdulmassih <abdalmassehsaba@gmail.com>
2023-08-27 11:09:25 +0200

    Added cross origin for front end

--- src/main/java/com/favouritemanager/api/controller/CategoryController.java
+++ src/main/java/com/favouritemanager/api/controller/CategoryController.java
@@ -4,15 +4,13 @@ import com.favouritemanager.api.dto.CategoryListItem;
 import com.favouritemanager.api.persistance.entity.Category;
 import com.favouritemanager.api.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/category")
+@CrossOrigin("http://127.0.0.1:5500")
 public class CategoryController {
     @Autowired
     private ICategoryService categoryService;
--- src/main/java/com/favouritemanager/api/controller/FavouriteContoller.java
+++ src/main/java/com/favouritemanager/api/controller/FavouriteContoller.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/favourite")
+@CrossOrigin("http://127.0.0.1:5500")
 public class FavouriteContoller {
     @Autowired
     private IFavouriteService favouriteService;
